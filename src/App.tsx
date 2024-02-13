4f8118c27625500268f964845458e47343eca5ad
soonamback <soonam.yi123@gmail.com>
2024-02-13 15:21:10 +0900

    add add task functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
+import { useState } from "react";
 import Goals from "./components/Goals.tsx";
 import Header from "./components/Header.tsx";
 import goalsImg from "./assets/goals.jpg"
+
+type Goals = {
+  title: string;
+  description: string;
+  id: number;
+}
+
 export default function App() {
+  const [goals, setGoals] = useState<Goals[]>([])
+  function handleAddGoal() {
+      setGoals(prevGoals => {
+        const newGoals: Goals = {
+          id: Math.random(),
+          title: 'Learn TS + React',
+          description: 'Learn it in depth'
+        };
+        return [...prevGoals, newGoals];
+      })
+  }
   return(
   <main>
     <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
-      <h1>my goals</h1>
+      <button onClick={handleAddGoal}>Button</button>
     </Header>
-  <Goals title="Learn React + TS">
-    <p>Learn it from ground up</p>
-    </Goals>
+  <ul>
+    {goals.map((goal) =>(
+      <li key={goal.id}>
+        <Goals title={goal.title}>
+          <p>{goal.description}</p>
+       </Goals>
+      </li>
+    ))}
+  </ul>
   </main>
   )
 }
