93a7d3ae8c246df0ee87d7fdb0b716f80db318cd
soonamback <soonam.yi123@gmail.com>
2024-02-13 11:27:08 +0900

    add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Goals from "./components/Goals.tsx";
-
+import Header from "./components/Header.tsx";
+import goalsImg from "./assets/goals.jpg"
 export default function App() {
   return(
   <main>
-  <Goals
-    title="Learn React + TS"
-    description="Learn it from the ground up"
-  />
+    <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
+      <h1>my goals</h1>
+    </Header>
+  <Goals title="Learn React + TS">
+    <p>Learn it from ground up</p>
+    </Goals>
   </main>
   )
 }
--- src/components/Goals.tsx
+++ src/components/Goals.tsx
@@ -1,18 +1,36 @@
+import {  type ReactNode  } from "react";
+
+interface GoalsProps {
+    title: string;
+    children: ReactNode
+}
 export default function Goals({
     title,
-    description
-}: {
-    title: string,
-    description: string
-} ){
+    children
+}: GoalsProps ){
 
     return (
     <article>
         <div>
             <h2>{ title }</h2>
-            <p>{ description }</p>
+            { children }
         </div>
         <button>Delete</button>
     </article>
     )
 }
+
+// type GoalsProps = PropsWithChildren<{ title: string }>;
+
+// const Goals: FC<GoalsProps> = ({ title, children }) => {
+//     return (
+//         <article>
+//             <div>
+//                 <h2>{title}</h2>
+//                 {children}
+//             </div>
+//                 <button>Delete</button>
+//         </article>
+//     )
+// }
+// export default Goals;
